c1e61e466605bb1b557311beaf038399e2093a37
Suryansh <suryansh22519@iiitd.ac.in>
2024-11-19 22:50:13 +0530

    mk

--- main.cpp
+++ main.cpp
@@ -71,6 +71,7 @@ int main() {
         cout<<"This matrix ain't symmetric"<<endl;
     }
 
+    cout<<vector_x_true<<endl;
 
     cout<<"Choose method:\n1. Jacobi\n2. Gauss-Seidel\n3. Conjugate Gradient\n4. GMRES\nEnter choice:"<<endl;
 
@@ -117,7 +118,7 @@ int main() {
 
     cout<<"Print the result?"<<endl;
     getline(cin, input);
-    cout << vector_x - vector_x_true<< endl;
+    cout << vector_x<< endl;
 
     return 0;
 }
